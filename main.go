fce6658736b0ad5f81fc90d046f444cc620ae428
prithviraj324 <pritvirajc.cs19@bmsce.ac.in>
2022-11-25 20:41:42 +0530

    new

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
